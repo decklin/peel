be6472b41f605bc87a1b6b428f3b345457776a7c
Decklin Foster <decklin@red-bean.com>
2008-09-15 20:30:11 -0400

    Splitting this package out from lastfmsubmitd

--- setup.py
+++ setup.py
@@ -3,26 +3,22 @@
 from distutils.core import setup
 
 setup(
-    name='lastfmsubmitd',
-    version='0.37',
-    description='Last.fm submit daemon and example MPD/MusicBrainz clients',
+    name='ears',
+    version='1.0.1',
+    description='MusicBrainz querying, CD-ripping, and Last.fm tools',
     author='Decklin Foster',
     author_email='decklin@red-bean.com',
-    url='http://www.red-bean.com/~decklin/software/lastfmsubmitd/',
+    url='http://www.red-bean.com/decklin/ears/',
     classifiers=[
         'Intended Audience :: End Users/Desktop',
         'Development Status :: 4 - Beta',
         'License :: MIT/X Consortium License',
         'Topic :: Multimedia :: Sound :: Players',
         'Operating System :: POSIX',
-        'Environment :: No Input/Output (Daemon)',
+        'Environment :: Console (Text Based)',
         'Programming Language :: Python',
         ],
-    package_dir = {'': 'lib'},
-    packages = ['lastfm'],
     scripts = [
-        'lastfmsubmitd',
-        'lastmp',
         'lastcd',
         'mbfind',
         'mbget',
@@ -31,12 +27,7 @@ setup(
         ],
     data_files=[
         ('share/man/man1', [
-            'doc/lastfmsubmitd.1',
-            'doc/lastmp.1',
             'doc/mbget.1',
-            ]),
-        ('lib/lastfmsubmitd', [
-            'lastfmsubmit',
             ]),
         ],
     )
